7ade4d6b615db2a0434772db0d9bbf2eb3592104
3gretski <3gretski@gmail.com>
2024-12-06 15:31:02 -0700

    Added all questions to drag and drop

--- A8EduApp/quizdragdropwidget.cpp
+++ A8EduApp/quizdragdropwidget.cpp
@@ -16,16 +16,34 @@ QuizDragDropWidget::QuizDragDropWidget(QWidget *parent, QuizModel *QuizModel)
     ui->answer2->setQuestionText("Nutrients are absorbed into your bloodstream in the _____.");
     ui->answer3->setQuestionText("_____ can help restore gut balance, particularly after illness or antibiotic use.");
     ui->answer4->setQuestionText("The _____ is home to your gut microbiome. It also absorbs water and helps process waste.");
+    ui->answer5->setQuestionText("_____ has been linked to autoimmune diseases, such as rheumatoid arthritis, and heart disease.");
+    ui->answer6->setQuestionText("Certain gut bacteria produce harmful chemicals like _____, which can cause cholesterol to build up.");
+    ui->answer7->setQuestionText("Food which lacks fiber, and contains many preservatives and additives are _____.");
+    ui->answer8->setQuestionText("Your gut and brain communicate via the _____.");
+    ui->answer9->setQuestionText("Your gut is home to trillions of tiny organisms (bacteria, viruses, fungi) called the _____.");
+    ui->answer10->setQuestionText("The liver, pancreas, and gallbladder produce important _____ and bile to aid in digestion.");
 
     connect(ui->answer1, &DropLabel::answerDropped, this, &QuizDragDropWidget::onAnswerDropped);
     connect(ui->answer2, &DropLabel::answerDropped, this, &QuizDragDropWidget::onAnswerDropped);
     connect(ui->answer3, &DropLabel::answerDropped, this, &QuizDragDropWidget::onAnswerDropped);
     connect(ui->answer4, &DropLabel::answerDropped, this, &QuizDragDropWidget::onAnswerDropped);
+    connect(ui->answer5, &DropLabel::answerDropped, this, &QuizDragDropWidget::onAnswerDropped);
+    connect(ui->answer6, &DropLabel::answerDropped, this, &QuizDragDropWidget::onAnswerDropped);
+    connect(ui->answer7, &DropLabel::answerDropped, this, &QuizDragDropWidget::onAnswerDropped);
+    connect(ui->answer8, &DropLabel::answerDropped, this, &QuizDragDropWidget::onAnswerDropped);
+    connect(ui->answer9, &DropLabel::answerDropped, this, &QuizDragDropWidget::onAnswerDropped);
+    connect(ui->answer10, &DropLabel::answerDropped, this, &QuizDragDropWidget::onAnswerDropped);
 
     quizModel->addDragDropQuestion("Your gut produces important vitamins like _____ and K.", "B12");
     quizModel->addDragDropQuestion("Nutrients are absorbed into your bloodstream in the _____.", "Small Intestine");
     quizModel->addDragDropQuestion("_____ can help restore gut balance, particularly after illness or antibiotic use.", "Probiotics");
     quizModel->addDragDropQuestion("The _____ is home to your gut microbiome. It also absorbs water and helps process waste.", "Large Intestine");
+    quizModel->addDragDropQuestion("_____ has been linked to autoimmune diseases, such as rheumatoid arthritis, and heart disease.", "Dysbiosis");
+    quizModel->addDragDropQuestion("Certain gut bacteria produce harmful chemicals like _____, which can cause cholesterol to build up.", "TMAO");
+    quizModel->addDragDropQuestion("Food which lacks fiber, and contains many preservatives and additives are _____.", "Processed foods");
+    quizModel->addDragDropQuestion("Your gut and brain communicate via the _____.", "Gut-brain Axis");
+    quizModel->addDragDropQuestion("Your gut is home to trillions of tiny organisms (bacteria, viruses, fungi) called the _____.", "Microbiome");
+    quizModel->addDragDropQuestion("The liver, pancreas, and gallbladder produce important _____ and bile to aid in digestion.", "Enzymes");
 }
 
 void QuizDragDropWidget::onAnswerDropped(const QString &question, const QString &answer)
